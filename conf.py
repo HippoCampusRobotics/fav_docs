9ffc7a22507c437433331544b4d1310230f8eb5a
josina1234 <josina.gerdes@tuhh.de>
2026-03-02 15:32:56 +0100

    exclude_pattern,  hide more outdated files

--- conf.py
+++ conf.py
@@ -46,7 +46,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv', '00_main_toc/final_project.rst', '00_main_toc/assignment_depth_control.rst', '00_main_toc/assignment_loc_and_control.rst']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv', '00_main_toc/final_project.rst', '00_main_toc/assignment_depth_control.rst', '00_main_toc/assignment_loc_and_control.rst', 'installation/install_firmware.rst']
 
 rst_prolog = """
 
